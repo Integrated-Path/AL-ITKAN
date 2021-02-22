371588f6e6aa29eb93c0463162b5de185c46eca3
M-Saeb <mohammed.saeb98@hotmail.com>
2021-02-22 18:55:13 +0000

    edited the original expense sheet approve function

--- models/expenses_sheet.py
+++ models/expenses_sheet.py
@@ -13,8 +13,25 @@ class HrExpensesSheet(models.Model):
     analytic_account_id = fields.Many2one("account.analytic.account", string="Budget", domain="[('company_id', 'in', [company_id, False] )]")
     expense_line_ids = fields.One2many(states={'done': [('readonly', True)], 'post': [('readonly', True)]})
 
+    # This is the original Function
     @api.constrains("analytic_account_id", "expense_line_ids")
     def _get_analytic_account(self):
         for report in self:
             for line in report.expense_line_ids:
                 line.analytic_account_id = report.analytic_account_id.id
+
+        def approve_expense_sheets(self):
+        if not self.user_has_groups('hr_expense.group_hr_expense_team_approver'):
+            raise UserError(_("Only Managers and HR Officers can approve expenses"))
+        elif not self.user_has_groups('hr_expense.group_hr_expense_manager'):
+            current_managers = self.employee_id.expense_manager_id | self.employee_id.parent_id.user_id | self.employee_id.department_id.manager_id.user_id
+
+            if self.employee_id.user_id == self.env.user:
+                raise UserError(_("You cannot approve your own expenses"))
+
+            # if not self.env.user in current_managers and not self.user_has_groups('hr_expense.group_hr_expense_user') and self.employee_id.expense_manager_id != self.env.user:
+            #     raise UserError(_("You can only approve your department expenses"))
+
+        responsible_id = self.user_id.id or self.env.user.id
+        self.write({'state': 'approve', 'user_id': responsible_id})
+        self.activity_update()
