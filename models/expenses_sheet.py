c6ddbc340aa35bfa6c8a1c5c1c66cbc3151aaf5e
ahmadn91 <ahmadnmajeed@gmail.com>
2021-02-22 19:20:14 +0000

    fixed indeentation

--- models/expenses_sheet.py
+++ models/expenses_sheet.py
@@ -20,14 +20,14 @@ class HrExpensesSheet(models.Model):
             for line in report.expense_line_ids:
                 line.analytic_account_id = report.analytic_account_id.id
 
-        def approve_expense_sheets(self):
+    def approve_expense_sheets(self):
         if not self.user_has_groups('hr_expense.group_hr_expense_team_approver'):
             raise UserError(_("Only Managers and HR Officers can approve expenses"))
         elif not self.user_has_groups('hr_expense.group_hr_expense_manager'):
             current_managers = self.employee_id.expense_manager_id | self.employee_id.parent_id.user_id | self.employee_id.department_id.manager_id.user_id
 
-            if self.employee_id.user_id == self.env.user:
-                raise UserError(_("You cannot approve your own expenses"))
+            # if self.employee_id.user_id == self.env.user:
+            #     raise UserError(_("You cannot approve your own expenses"))
 
             # if not self.env.user in current_managers and not self.user_has_groups('hr_expense.group_hr_expense_user') and self.employee_id.expense_manager_id != self.env.user:
             #     raise UserError(_("You can only approve your department expenses"))
