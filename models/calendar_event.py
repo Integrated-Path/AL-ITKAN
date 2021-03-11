d2e151f885bc4cf89d1b31bd7ceb24ab252fff25
M.Saeb <48291783+M-Saeb@users.noreply.github.com>
2021-03-11 12:29:55 +0300

    Changed Default send_email checkbox field

--- models/calendar_event.py
+++ models/calendar_event.py
@@ -3,7 +3,7 @@ from odoo.exceptions import UserError
 
 class CalendarEvent(models.Model):
     _inherit = "calendar.event"
-    send_email_to_attendees = fields.Boolean(string="Send Email", default=True)
+    send_email_to_attendees = fields.Boolean(string="Send Email")
 
     def create_attendees(self):
         self = self.with_context(detaching= (not self.send_email_to_attendees) )
