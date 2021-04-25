5affc3286c715a5badd165ae444e747b3982dccc
M-Saeb <mohammed.saeb98@hotmail.com>
2021-04-25 10:05:48 +0000

    commented res.partner create function

--- models/res_partner.py
+++ models/res_partner.py
@@ -7,16 +7,16 @@ class ResPartner(models.Model):
 
     # N.A Record id is => 21362
 
-    @api.model
-    def create(self, values):
-        """ Itakn DataBase is suffering from an issue with creating record.
-        This function is to handle that error """
-        na_record = self.env['res.partner'].browse([21362])
-        if values.get("name", False):
-            result = super(ResPartner, self).create(values)    
-            return result
-        else:
-            return na_record
+    # @api.model
+    # def create(self, values):
+    #     """ Itakn DataBase is suffering from an issue with creating record.
+    #     This function is to handle that error """
+    #     na_record = self.env['res.partner'].browse([21362])
+    #     if values.get("name", False):
+    #         result = super(ResPartner, self).create(values)    
+    #         return result
+    #     else:
+    #         return na_record
 
     def unlink(self):
         for line in self:
