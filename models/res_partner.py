79ab0a9aef1a20fea50b2667c8862e97d6dc43c2
M.Saeb <48291783+M-Saeb@users.noreply.github.com>
2021-05-09 10:37:38 +0300

    fixed api issue

--- models/res_partner.py
+++ models/res_partner.py
@@ -5,24 +5,27 @@ from odoo.exceptions import UserError
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    # N.A Record id is => 21362
-
-    # @api.model
-    # def create(self, values):
-    #     """ Itakn DataBase is suffering from an issue with creating record.
-    #     This function is to handle that error """
-    #     na_record = self.env['res.partner'].browse([21362])
-    #     if values.get("name", False):
-    #         result = super(ResPartner, self).create(values)    
-    #         return result
-    #     else:
-    #         return na_record
-
-#     def unlink(self):
-#         for line in self:
-#             if line.id == 21362:
-#                 raise UserError(_("You can not delete this contact cause it is being used by the support team"))
-#             else:
-#                 res = super(ResPartner, line).unlink()
-#                 return res
+    # N.A Record id is => 25477
+    # APIBOT user is => 64
     
+    @api.model
+    def create(self, values):
+        """ Itakn DataBase is suffering from an issue with creating record.
+        This function is to handle that error """
+        na_record = self.env['res.partner'].browse([25477])
+        api_id = 64
+        if not values.get("name") and self.env.user.id == api_id:
+            return na_record
+        else:
+            result = super(ResPartner, self).create(values)
+            return result
+
+    def unlink(self):
+        for contact in self:
+            if contact.id == 25477:
+                raise UserError(_("You can not delete this contact cause it is being used by the support team"))
+            else:
+                pass
+
+        res = super(ResPartner, self).unlink()
+        return res
